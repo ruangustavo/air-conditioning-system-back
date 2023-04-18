e61b8d859811b9d568cf72c0c777092f376f4846
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-04-18 18:34:03 -0300

    feat: remove toggled attribute from database

---
+++ prisma/migrations/20230418213200_remove_toggled_attribute/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `toggled` on the `air_conditioners` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "air_conditioners" DROP COLUMN "toggled";

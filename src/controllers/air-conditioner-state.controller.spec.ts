8663ff08f6505d21abbf98992e420c4f1c6c9461
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-08-13 11:41:28 -0300

    test: handle invalid and not-existent air conditioner state

--- src/controllers/air-conditioner-state.controller.spec.ts
+++ src/controllers/air-conditioner-state.controller.spec.ts
@@ -19,4 +19,38 @@ describe('AirConditionerStateController (e2e)', () => {
 
     expect(response.body).toEqual({ success: true })
   })
+
+  it('should handle not-existent air conditioner', async () => {
+    const nonExistentId = 9999
+
+    const response = await request(app)
+      .put(`/air-conditioners/${nonExistentId}/state`)
+      .send({ state: true })
+      .expect(404)
+
+    expect(response.body).toEqual({ error: 'Resource not found' })
+  })
+
+  it('should handle invalid state', async () => {
+    const airConditioner = await prisma.airConditioner.create({
+      data: {
+        model: 'Air-conditioner',
+        brand: 'Brand'
+      }
+    })
+
+    const response = await request(app)
+      .put(`/air-conditioners/${airConditioner.id}/state`)
+      .send({ state: 'invalid' })
+
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('should handle invalid id', async () => {
+    const response = await request(app)
+      .put('/air-conditioners/invalid-id/state')
+      .send({ state: true })
+
+    expect(response.statusCode).toBe(400)
+  })
 })
